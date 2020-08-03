db7e11a1d1176d7bf02ef5937579eb2032ef9233
abdul diallo <rahimi.diallo96@gmail.com>
2020-08-03 18:24:30 -0400

    add logo and pin it

--- Choizi/Controllers/Authentication/Login.swift
+++ Choizi/Controllers/Authentication/Login.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class Login : UIViewController {
     
+    private var logo : UIImageView = {
+        let img = UIImageView()
+        img.image = #imageLiteral(resourceName: "app_icon").withRenderingMode(.alwaysTemplate)
+        img.tintColor = .white
+        return img
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configUI()
+    }
+    
+}
+
+//MARK: - helpers
+
+extension Login {
+    
+    func configUI() {
+        navigationController?.navigationBar.isHidden = true
+        view.addSubview(logo)
+        logo.setDimensions(height: 100, width: 100)
+        logo.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 10)
+        logo.centerX(inView: view)
     }
     
 }
