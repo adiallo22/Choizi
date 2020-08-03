4a39a5f9ef7ded69ae300f7304bf998ccafc5a07
abdul diallo <rahimi.diallo96@gmail.com>
2020-08-03 13:52:39 -0400

    user model

---
+++ Choizi/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Choizi
+//
+//  Created by Abdul Diallo on 8/3/20.
+//  Copyright © 2020 Abdul Diallo. All rights reserved.
+//
+
+import UIKit.UIImage
+
+struct User {
+    
+    var name : String
+    var age : Int
+    var photos : [UIImage]
+    
+}
