abdfa98c357d019372af3d57238bb6a77a7b1923
abdul diallo <rahimi.diallo96@gmail.com>
2020-08-03 18:19:33 -0400

    embbed login in a nav controller and make it root

---
+++ Choizi/Controllers/Authentication/Login.swift
@@ -0,0 +1,17 @@
+//
+//  Login.swift
+//  Choizi
+//
+//  Created by Abdul Diallo on 8/3/20.
+//  Copyright © 2020 Abdul Diallo. All rights reserved.
+//
+
+import UIKit
+
+class Login : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Choizi/Controllers/Authentication/SingUp.swift
@@ -0,0 +1,17 @@
+//
+//  SingUp.swift
+//  Choizi
+//
+//  Created by Abdul Diallo on 8/3/20.
+//  Copyright © 2020 Abdul Diallo. All rights reserved.
+//
+
+import UIKit
+
+class SignUp : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
