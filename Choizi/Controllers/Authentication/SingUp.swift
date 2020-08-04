04130695fb3c12a5107904dd309aa19a19c44d15
abdul diallo <rahimi.diallo96@gmail.com>
2020-08-03 21:27:11 -0400

    add photo button

--- Choizi/Controllers/Authentication/SingUp.swift
+++ Choizi/Controllers/Authentication/SingUp.swift
@@ -10,8 +10,40 @@ import UIKit
 
 class SignUp : UIViewController {
     
+    private var photoButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+        button.addTarget(self, action: #selector(imagePicked), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configUI()
+    }
+    
+}
+
+//MARK: - helpers
+
+extension SignUp {
+    
+    func configUI() {
+        view.addSubview(photoButton)
+        photoButton.setDimensions(height: 270, width: 270)
+        photoButton.centerX(inView: view)
+        photoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 24)
+    }
+    
+}
+
+//MARK: - selectors
+
+extension SignUp {
+    
+    @objc func imagePicked() {
+        print("picked..")
     }
     
 }
