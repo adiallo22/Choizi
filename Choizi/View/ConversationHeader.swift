b9f8ad3c32c3abde70881260f27a2f70b8863ef4
abdul diallo <rahimi.diallo96@gmail.com>
2020-09-05 12:41:22 -0400

    add collection view to conversation header and anchor it

--- Choizi/View/ConversationHeader.swift
+++ Choizi/View/ConversationHeader.swift
@@ -18,17 +18,43 @@ class ConversationHeader : UIView {
         return label
     }()
     
+    private lazy var collectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView.init(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .orange
+        return cv
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(label)
-        label.anchor(top: topAnchor,
-                     left: leftAnchor,
-                     paddingTop: 8,
-                     paddingLeft: 12)
+        configUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+//MARK: - helpers
+
+extension ConversationHeader {
+    fileprivate func configUI() {
+        addSubview(label)
+        label.anchor(top: topAnchor,
+                     left: leftAnchor,
+                     paddingTop: 8,
+                     paddingLeft: 12)
+        //
+        addSubview(collectionView)
+        collectionView.anchor(top: label.bottomAnchor,
+                              left: leftAnchor,
+                              bottom: bottomAnchor,
+                              right: rightAnchor,
+                              paddingTop: 6,
+                              paddingLeft: 12,
+                              paddingBottom: 6,
+                              paddingRight: 12)
+    }
 }
