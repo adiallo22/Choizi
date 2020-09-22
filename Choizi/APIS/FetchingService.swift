d9d7a0f2e2bcd63d05fa29dd3565413fbef48cdb
abdul diallo <rahimi.diallo96@gmail.com>
2020-09-22 15:50:58 -0400

    refractor

--- Choizi/APIS/FetchingService.swift
+++ Choizi/APIS/FetchingService.swift
@@ -12,21 +12,6 @@ import FirebaseAuth
 
 let ref = Storage.storage()
 
-protocol ServiceDownloadInterface {
-    func fetchUser(withUid uid: String, completion: @escaping(Result<User, Error>)->Void)
-    func fetchAllUsers(fromCurrentUser user: User, completion: @escaping(Result<[User], Error>)->Void)
-    func isThereAMatch(withUser user: User, completion: @escaping(Bool)->Void)
-    func fetchLikedUser(completion: @escaping([User]?) -> Void)
-    func fetchMatches(completion: @escaping([Match]) -> Void)
-}
-
-protocol ServiceUploadInterface {
-    func uploadImage(image: UIImage, completion: @escaping(Result<String, Error>)->Void)
-    func saveData(withUser user: User, completion: @escaping(Error?)->Void)
-    func saveSwipe(onUser user: User, isLike like: Bool, completion: @escaping(Error?)->Void)
-    func uploadMatch(currentUser: User, matchedUser: User)
-}
-
 struct Service : ServiceDownloadInterface {
     
     func uploadImage(image: UIImage, completion: @escaping(Result<String, Error>)->Void) {
